baf7881ccd1788b10e66051553960eabfc61d919
HIRO OMURA <i.t.a.n.o.m.t.h.u.b@gmail.com>
2025-11-15 11:06:58 +0900

    全部反映

--- js/main.js
+++ js/main.js
@@ -168,3 +168,30 @@ overlay.on('click', function () { //背景をクリックしたら
   dots: true,
   });
 }
+
+{
+  $(function(){
+	// 変数に要素を入れる
+	var open = $('.modal-open'),
+		close = $('.modal-close'),
+		container = $('.modal-container');
+
+	//開くボタンをクリックしたらモーダルを表示する
+	open.on('click',function(){	
+		container.addClass('active');
+		return false;
+	});
+
+	//閉じるボタンをクリックしたらモーダルを閉じる
+	close.on('click',function(){	
+		container.removeClass('active');
+	});
+
+	//モーダルの外側をクリックしたらモーダルを閉じる
+	$(document).on('click',function(e) {
+		if(!$(e.target).closest('.modal-body').length) {
+			container.removeClass('active');
+		}
+	});
+});
+}
